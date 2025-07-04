f929763f69b7fe32cbb485aa0143e66d2310254e
Y. Benjamin Perez C. <y.benjamin_pc@hotmail.com>
2025-07-04 12:12:34 +0000

    Added .py scripts to handle creating TCP clients to dsend data to Thignspeak, and created flie for sending data over UART protocol

---
+++ ThingSpeak-IoT-Arduino-Project/Server.py
@@ -0,0 +1,13 @@
+import socket
+
+clientsocket = socket.AF_INET, socket.SOCK_STREAM);
+
+clientSocket.connect(("127.0.0.1"));
+
+data = "Hello Server!";
+
+clientSocket.send(data.encode());
+
+dataFromServer = clientSocket.rec(1024);
+
+print(dataFromServer.decode());
---
+++ ThingSpeak-IoT-Arduino-Project/UART.py
@@ -0,0 +1,19 @@
+import serial
+
+# configure the serial port
+ser = serial.Serial('COM1', 9600, timeout=1)
+
+# pack the data
+data = [0x01, 0x02, 0x03]
+packed_data = bytes(data)
+
+# send the data
+ser.write(packed_data)
+
+# receive the data
+received_data = ser.read(3)
+
+# unpack the data
+unpacked_data = list(received_data)
+
+print(unpacked_data)
